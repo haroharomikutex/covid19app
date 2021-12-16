cc362a3f07ecd2cf6a98ec15b491e2a837b408a3
haroharomikutex <takahasimiku200@yahoo.co.jp>
2021-12-16 21:30:47 +0900

    一部改良

--- app/src/main/java/cf/mtjp/haroharo/MainActivity.java
+++ app/src/main/java/cf/mtjp/haroharo/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
 				pop.setIcon(R.drawable.attention);
 				pop.setCancelable(false);
 				pop.setInverseBackgroundForced(false);
-				pop.setMessage("ネットワークの状況が悪い或いは技術的なエラーが発生しています。\n再読み込みボタンを押して再読み込みを行ってください。\n改善が見られない場合は一度端末を再起動してみてください。");
+				pop.setMessage("ネットワークの状況が悪い或いは技術的なエラーが発生しています。\n端末がネットワークに接続されている事を確認し\n再読み込みボタンを押して再読み込みを行ってください。\n");
 				pop.setPositiveButton("再読み込み", new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface _dialog, int _which) {
@@ -145,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
 		vib.vibrate((long) (100));
 		pop.setTitle("ようこそ");
 		pop.setIcon(R.drawable.image_10);
-		pop.setMessage("【開発者からのお知らせ】\n静岡県のアプリコンテストで見事、本アプリが優秀賞を受賞しました！\n今後本格的な実用化に向けて様々な機関と連携して開発して参ります！\n==============\nこの度はシズスマ！(静岡スマート情報ポータルアプリ)をダウンロード頂きありがとうございます。\nこのアプリは新型コロナウイルスによって打撃を受けた静岡県の経済活動を活性化するとともに食品ロスの削減や自然豊かな静岡県の魅力を県内外にPRするアプリです。\n今後アプリに関するアナウンスはこちらのTwitterアカウント（ https://mobile.twitter.com/Shizuoka_COVID ）より配信いたしますのでフォローの程よろしくお願い致します。\n\n==============\nバージョン8.0.0 \n");
+		pop.setMessage("【開発者からのお知らせ】\n静岡県のアプリコンテストで見事、本アプリが優秀賞を受賞しました！\n今後本格的な実用化に向けて様々な機関と連携して開発して参ります！\n==============\nこの度はシズスマ！(静岡スマート情報ポータルアプリ)をダウンロード頂きありがとうございます。\nこのアプリは新型コロナウイルスによって打撃を受けた静岡県の経済活動を活性化するとともに食品ロスの削減や自然豊かな静岡県の魅力を県内外にPRするアプリです。\n今後アプリに関するアナウンスはこちらのTwitterアカウント（ https://mobile.twitter.com/Shizuoka_COVID ）より配信いたしますのでフォローの程よろしくお願い致します。\n\n==============\nバージョン9.0.0 \n");
 		pop.setPositiveButton("スタート", new DialogInterface.OnClickListener() {
 			@Override
 			public void onClick(DialogInterface _dialog, int _which) {
